4f12e3b53d3633d0dd9a35b9a0a503087b08c77e
Bjørn Gilstad <bjorn.gilstad@acando.no>
2019-02-25 21:35:40 +0100

    Make somewhat responsive

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 
 const BAR_WIDTH_MAX=500
 const BAR_WIDTH_MIN=100
+const NUMBER_OF_ROWS=10
 
 const barStyle = (width, color) => ({
   width: `${width}px`,
   background: color,
 })
 
-const funnyRows = (rows) => {
-  return Array(rows).fill().map(() => (<div className='bars'>{funnyBars(10)}</div>))
+const funnyRows = (rowHeight, bars) => {
+  return Array(NUMBER_OF_ROWS).fill().map(() => (
+    <div className='bars' style={{height: rowHeight}}>
+      {funnyBars(bars)}
+    </div>))
 }
 
 const funnyBars = (bars) => {
@@ -18,11 +22,22 @@ const funnyBars = (bars) => {
 }
 
 const App = () => {
+  const [rowHeight, setRows] = useState(Math.ceil(window.innerHeight / NUMBER_OF_ROWS))
+  const [bars, setBars] = useState(Math.ceil(window.innerWidth / (BAR_WIDTH_MAX/2)))
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setRows(Math.ceil(window.innerHeight / NUMBER_OF_ROWS))
+      setBars(Math.ceil(window.innerWidth / (BAR_WIDTH_MAX/2)))
+    });
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        {funnyRows(20)}
-      </header>
+      <div className="text">
+        Weee!
+      </div>
+        {funnyRows(rowHeight, bars)}
     </div>
   );
 }
@@ -31,7 +46,7 @@ const getRandomColorHex = () => '#'+Math.floor(Math.random()*16777215).toString(
 const getRandomIntBetween = (from, to) => Math.floor(Math.random()*(to-from)+from)
 
 const FunnyBar = () => {
-  const [barWidth, setWidth] = useState(getRandomIntBetween(BAR_WIDTH_MIN, BAR_WIDTH_MAX))
+  const [barWidth, setWidth] = useState(0)
   const [color, setColor] = useState(getRandomColorHex())
 
   useEffect(() => {
@@ -46,8 +61,7 @@ const FunnyBar = () => {
   })
 
   return (
-    <span style={barStyle(barWidth, color)} className='bar'>
-    </span>
+    <span style={barStyle(barWidth, color)} className='bar' />
   )
 }
 
